c0aee88a11506a76829c5809671c4261813cf958
Ryan Schachte <code@ryan-schachte.com>
2019-06-20 16:01:52 -0700

    Adding team logic updates

--- data/teams.js
+++ data/teams.js
@@ -2,10 +2,49 @@ class Teams {
   static getTeams() {
     return {
       teams: {
-        team1: { members: ["john"], limit: 0 },
-        team2: { members: [], limit: 0 },
-        team3: { members: ["schachte"], limit: 2 },
-        team4: { members: [], limit: 0 }
+        fears: {
+          members: [
+            "lambeaux",
+            "mojogitoverhere",
+            "brhumphe",
+            "aj-brooks",
+            "kentmorrissey",
+            "brianfelix",
+            "adimka"
+          ],
+          limit: 4
+        },
+        scrumbledore: {
+          members: [
+            "willwill96",
+            "corey-collins",
+            "andrewzimmer",
+            "hayleynorton",
+            "zta6"
+          ],
+          limit: 4
+        },
+        rebel: {
+          members: [
+            "schachte",
+            "mcat97",
+            "samuelechu",
+            "nsuvarna",
+            "gordocanchola"
+          ],
+          limit: 4
+        },
+        scrumdog: {
+          members: [
+            "jrnorth",
+            "gjvera",
+            "bennuttle",
+            "leo-sakh",
+            "bernardIgiri",
+            "lavoywj"
+          ],
+          limit: 4
+        }
       }
     };
   }
